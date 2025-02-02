0e14ca5a0a72fe00147ad0cf7e8a879c107a4292
ssaauudd22 <saud.manganhar01@utrgv.edu>
2025-02-02 23:34:08 +0000

    this one was easy, but a lot of going back & forth to see if I typed it correctly. :)

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,28 @@
+public class Converter{
+    public static void main (String[] args){
+    // Convert the string strFloat into a float.
+    String strFloat = "374.25";
+    float from_strFloat = Float.parseFloat(strFloat); // assign here
+    System.out.println(strFloat);
 
+    // Convert the string strInt into an integer.
+    String strInt = "1250";
+    int from_strInt = Integer.parseInt(strInt); // assign here
+    System.out.println(strInt);
+
+    // Convert the float floatNum into an integer.
+    float floatNum = 56.715f;
+    int from_floatNum = (int) floatNum; // assign here
+    System.out.println(floatNum);
+
+    // Convert the integer intNum into a float.
+    int intNum = 1500;
+    float from_intNum = (float) intNum; // assign here
+    System.out.println(intNum);
+
+    // Convert the double doubleNum into a string.
+    double doubleNum = 3.14159265;
+    String from_doubleNum = "" + doubleNum; // assign here
+    System.out.println(doubleNum);
+    }
+}
